2a9ecb3eb1d35b21b60a16e071d9fd57db7e434f
gaiiouch <ag.mauger@gmail.com>
2024-03-17 14:23:14 +0100

    Exemple

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// server.js
+
+// first we import our dependencies…
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// and create our instances
+const app = express();
+const router = express.Router();
+
+// set our port to either a predetermined port number if you have set it up, or 3001
+const API_PORT = process.env.API_PORT || 3001;
+// now we should configure the API to use bodyParser and look for JSON data in the request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// now we can set the route path & initialize the API
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+// Use our router configuration when we call /api
+app.use('/api', router);
+
+app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
